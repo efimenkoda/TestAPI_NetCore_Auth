5a64119909f7e98137c94405ddde8f4f324583bd
efimenkoda <dimas86@bk.ru>
2020-06-05 18:21:02 +0500

    unhandler

---
+++ TestAPI_NetCore_Auth/TestAPI_NetCore_Auth/Controllers/UnhExceptionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestAPI_NetCore_Auth.Filters;
+
+namespace TestAPI_NetCore_Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [UnhandledException]
+    public class UnhExceptionController : ControllerBase
+    {
+        public async Task<IActionResult> Get()
+        {
+            int x = 0;
+            int y = 8 / x;
+            return Ok(y);
+
+        }
+    }
+}
---
+++ TestAPI_NetCore_Auth/TestAPI_NetCore_Auth/Filters/UnhandledException.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAPI_NetCore_Auth.Filters
+{
+    public class UnhandledException : Attribute, IExceptionFilter
+    {
+        public async void OnException(ExceptionContext context)
+        {
+            //if (context.Exception != null)
+            //{
+            //    byte[] data = Encoding.UTF8.GetBytes( "error:unhandled exception");
+            //    context.HttpContext.Response.ContentType = "application/json";
+            //    await context.HttpContext.Response.Body.WriteAsync(data,0,data.Length);
+
+            //}
+            //context.Result = new ContentResult
+            //{
+            //    Content = "error:unhandled exception"
+            //};
+            string actionName = context.ActionDescriptor.DisplayName;
+            string exceptionStack = context.Exception.StackTrace;
+            string exceptionMessage = context.Exception.Message;
+            context.Result = new ContentResult
+            {
+                Content = $"error:unhandled exception"
+            };
+            context.ExceptionHandled = true;
+
+        }
+    }
+}
